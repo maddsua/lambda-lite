645fce86e21feb198b7a4434d414c200ffe19caf
maddsua <maddsuadev@proton.me>
2023-12-29 00:56:43 +0200

    server -> adapter

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import type { RouteConfig, RouteHandler } from "./lib/middleware/middleware.types.ts";
 import { JSONResponse } from "./lib/api/jsonResponse.ts";
-import { startServer } from "./lib/server/deno/server.ts";
+import { startServer } from "./lib/adapters/deno/server.ts";
 import * as envutils from './lib/util/envutils.ts';
 
 export {
