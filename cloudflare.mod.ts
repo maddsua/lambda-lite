020dd77499555c8529ff4409daf9421d358fe68b
maddsua <maddsuadev@proton.me>
2024-01-09 04:25:25 +0200

    Update cloudflare.mod.ts

--- cloudflare.mod.ts
+++ cloudflare.mod.ts
@@ -1,6 +1,7 @@
 
 import type { RouteConfig, RouteHandler } from "./lib/middleware/route.types.ts";
 import { JSONResponse } from "./lib/rest/jsonResponse.ts";
+import { LambdaMiddleware } from "./npm.mod.ts";
 import { workerFetchHandler, type StartServerOptions } from "./lib/adapters/cloudflare/worker.ts";
 import * as envutils from './lib/util/envutils.ts';
 
@@ -8,6 +9,7 @@ export {
 	RouteHandler,
 	RouteConfig,
 	JSONResponse,
+	LambdaMiddleware,
 	workerFetchHandler,
 	StartServerOptions,
 	envutils,
