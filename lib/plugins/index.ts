b5008647d3f65da5f100dc6a5efbeb4adaa3bbc3
maddsua <maddsuadev@proton.me>
2024-01-10 21:46:30 +0200

    Create index.ts

---
+++ lib/plugins/index.ts
@@ -0,0 +1,11 @@
+import { cors } from "./cors.ts";
+import { ratelimiter } from "./ratelimiter.ts";
+import { serviceTokenChecker } from "./serviceTokenChecker.ts";
+import { methodChecker } from "./methodChecker.ts";
+
+export {
+	cors,
+	ratelimiter,
+	serviceTokenChecker,
+	methodChecker
+};
