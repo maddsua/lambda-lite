ccd4735287c368e757f60435698622423dcac826
maddsua <maddsuadev@proton.me>
2024-01-10 11:09:47 +0200

    Update middleware.ts

--- lib/middleware/middleware.ts
+++ lib/middleware/middleware.ts
@@ -4,14 +4,11 @@ import type { RouteHandler, RouterRoutes } from './route.ts';
 import { JSONResponse } from '../rest/jsonResponse.ts';
 import { ServiceConsole } from '../util/console.ts';
 import { getRequestIdFromProxy, generateRequestId } from '../util/misc.ts';
-import { MiddlewarePlugins } from './plugins.ts';
+import { PluginGenerator } from './plugins.ts';
 
 interface HandlerCtx {
 	expandPath?: boolean;
-	plugins?: {
-		before?: MiddlewarePlugins;
-		after?: MiddlewarePlugins;
-	};
+	plugins?: PluginGenerator[];
 	handler: RouteHandler;
 };
 
@@ -19,10 +16,6 @@ export class LambdaMiddleware {
 
 	config: Partial<MiddlewareOptions>;
 	handlersPool: Record<string, HandlerCtx>;
-	plugins?: {
-		before?: MiddlewarePlugins;
-		after?: MiddlewarePlugins;
-	};
 
 	constructor (routes: RouterRoutes, config?: Partial<MiddlewareOptions>) {
 
@@ -30,17 +23,6 @@ export class LambdaMiddleware {
 
 		//	transform routes
 		this.handlersPool = {};
-
-		//	setup plugins
-		if (this.config.plugins) {
-
-			this.plugins = {
-				before: this.config?.plugins?.filter(item => item.sequernce === 'before'),
-				after: this.config?.plugins?.filter(item => item.sequernce === 'after')
-			};
-
-			delete this.config.plugins;
-		}
 
 		for (const route in routes) {
 
@@ -58,21 +40,10 @@ export class LambdaMiddleware {
 			};
 
 			//	setup route plugins
-			if (routeCtx.plugins || this.plugins) {
-
-				const localBefore =  routeCtx.plugins?.filter(item => item.sequernce === 'before');
-				const localAfter = routeCtx.plugins?.filter(item => item.sequernce === 'after');
-
-				const localBeforeSet = new Set<string>(localBefore?.map(item => item.instance.id));
-				const localAfterSet = new Set<string>(localAfter?.map(item => item.instance.id));
-
-				const globalBefore = localBefore?.length ? this.plugins?.before?.filter(item => !localBeforeSet.has(item.instance.id)) : this.plugins?.before;
-				const globalAfter = localAfter?.length ? this.plugins?.after?.filter(item => !localAfterSet.has(item.instance.id)) : this.plugins?.after;
-
-				handlerCtx.plugins = {
-					before: (globalBefore || []).concat(localBefore || []),
-					after: (globalAfter || []).concat(localAfter || []),
-				};
+			if (routeCtx.plugins || this.config.plugins) {
+				const handlerPluginsSet = new Set<string>(routeCtx.plugins?.map(item => item.id));
+				const applyGlobal = this.config.plugins?.filter(item => !handlerPluginsSet.has(item.id));
+				handlerCtx.plugins = (applyGlobal || []).concat(routeCtx.plugins || []);
 			}
 
 			const applyHandlerPath = route.replace(/\/\*?$/i, '');
@@ -97,9 +68,6 @@ export class LambdaMiddleware {
 		const requestID = getRequestIdFromProxy(request.headers, this.config.proxy?.requestIdHeader) || generateRequestId();
 		const clientIP = ((this.config.proxy?.forwardedIPHeader ? request.headers.get(this.config.proxy.forwardedIPHeader) : undefined)) || info.hostname;
 
-		const requestOrigin = request.headers.get('origin');
-		const handleCORS = this.config.handleCORS !== false;
-		let allowedOrigin: string | null = null;
 		let requestDisplayUrl = '/';
 
 		const console = new ServiceConsole(requestID);
@@ -177,64 +145,101 @@ export class LambdaMiddleware {
 				requestInfo,
 			});
 
+			const runPlugins = routectx.plugins?.map(item => item.spawn());
+
 			//	run "before" plugins
-			let modifiedReqest: Request | null = null;
-			if (routectx.plugins?.before) {
+			let pluginModifiedReqest: Request | null = null;
+			let pluginBeforeResponse: Response | null = null;
 
-				for (const plugin of routectx.plugins.before) {
+			if (runPlugins?.length) {
 
-					const temp = plugin.instance.execute({
-						request,
+				for (const plugin of runPlugins) {
+
+					if (!plugin.executeBefore) continue;
+
+					const temp = await plugin.executeBefore({
+						request: pluginModifiedReqest || request,
 						info: requestInfo,
-						response: null,
 						middleware: this
 					});
 
-					if (!temp) continue;
+					if (temp?.modifiedRequest) {
+						pluginModifiedReqest = temp.modifiedRequest;
+					}
+
+					if (temp?.overrideResponse) {
+						pluginBeforeResponse = temp.overrideResponse;
+						break;
+					}
 				}
 			}
 
 			//	execute route function
-			try {
+			if (!pluginBeforeResponse) pluginBeforeResponse = await (async () => {
 
-				const handlerResponse = await routectx.handler(request, requestContext);
+				try {
 
-				//	here we convert a non-standard response object to a standard one
-				//	all non standard should provide a "toResponse" method to do that
-				const responseObject = handlerResponse instanceof Response ? handlerResponse : handlerResponse.toResponse();
-
-				//	and if after that it's still not a Response we just crash the request
-				if (!(responseObject instanceof Response)) {
-					const typeErrorReport = (handlerResponse && typeof handlerResponse === 'object') ?
-						`object keys ({${Object.keys(handlerResponse).join(', ')}}) don't match handler response interface` :
-						`variable of type "${typeof handlerResponse}" is not a valid handler response`;
-					throw new Error('Invalid function response: ' + typeErrorReport);
+					const handlerResponse = await routectx.handler(request, requestContext);
+	
+					//	here we convert a non-standard response object to a standard one
+					//	all non standard should provide a "toResponse" method to do that
+					const responseObject = handlerResponse instanceof Response ? handlerResponse : handlerResponse.toResponse();
+	
+					//	and if after that it's still not a Response we just crash the request
+					if (!(responseObject instanceof Response)) {
+						const typeErrorReport = (handlerResponse && typeof handlerResponse === 'object') ?
+							`object keys ({${Object.keys(handlerResponse).join(', ')}}) don't match handler response interface` :
+							`variable of type "${typeof handlerResponse}" is not a valid handler response`;
+						throw new Error('Invalid function response: ' + typeErrorReport);
+					}
+	
+					return responseObject;
+	
+				} catch (error) {
+	
+					console.error('Lambda middleware error:', (error as Error | null)?.message || error);
+	
+					switch (this.config.defaultResponses?.error) {
+	
+						case 'log': return new JSONResponse({
+							error_text: 'unhandled middleware error',
+							error_log: (error as Error | null)?.message || JSON.stringify(error)
+						}, { status: 500 }).toResponse();
+	
+						default: return new JSONResponse({
+							error_text: 'unhandled middleware error'
+						}, { status: 500 }).toResponse();
+					}
 				}
 
-				return responseObject;
+			})();
 
-			} catch (error) {
+			if (runPlugins?.length) {
 
-				console.error('Lambda middleware error:', (error as Error | null)?.message || error);
+				for (const plugin of runPlugins) {
 
-				switch (this.config.defaultResponses?.error) {
+					if (!plugin.executeAfter) continue;
 
-					case 'log': return new JSONResponse({
-						error_text: 'unhandled middleware error',
-						error_log: (error as Error | null)?.message || JSON.stringify(error)
-					}, { status: 500 }).toResponse();
+					const temp = await plugin.executeAfter({
+						request: pluginModifiedReqest || request,
+						response: pluginBeforeResponse,
+						info: requestInfo,
+						middleware: this
+					});
 
-					default: return new JSONResponse({
-						error_text: 'unhandled middleware error'
-					}, { status: 500 }).toResponse();
+					if (temp?.overrideResponse) {
+						if (temp.chainable === false) return temp.overrideResponse;
+						pluginBeforeResponse = temp.overrideResponse;
+					}
 				}
 			}
+
+			return pluginBeforeResponse;
 
 		})();
 
 		//	add some headers so the shit always works
-		routeResponse.headers.set('x-powered-by', 'maddsua/lambda-lite');
-		if (allowedOrigin) routeResponse.headers.set('Access-Control-Allow-Origin', allowedOrigin);
+		routeResponse.headers.set('x-powered-by', 'maddsua/lambda');
 		routeResponse.headers.set('x-request-id', requestID);
 
 		//	log for, you know, reasons
