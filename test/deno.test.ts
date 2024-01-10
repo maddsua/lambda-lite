917f4163e9e67a8f50ffbc2abd464420727b814c
maddsua <maddsuadev@proton.me>
2024-01-10 13:25:36 +0200

    Update deno.test.ts

--- test/deno.test.ts
+++ test/deno.test.ts
@@ -1,9 +1,13 @@
 import { startServer } from "../deno.mod.ts";
+import { methodChecker } from "../lib/plugins/methodChecker.ts";
 
 await startServer({
 	serve: {
 		port: 8080,
 	},
 	routesDir: 'test/functions',
-	healthcheckPath: '/health'
+	healthcheckPath: '/health',
+	plugins: [
+		methodChecker(['POST'])
+	]
 });
