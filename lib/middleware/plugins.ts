abde8c6ab00a2147c014c81e4d523c80534e7d73
maddsua <maddsuadev@proton.me>
2024-01-10 09:47:35 +0200

    Update plugins.ts

--- lib/middleware/plugins.ts
+++ lib/middleware/plugins.ts
@@ -1,7 +1,24 @@
+import type { LambdaMiddleware } from './middleware.ts';
 
-export interface MiddlewarePluginOptions {
-	paths?: string | RegExp;
-	sequernce?: 'after' | 'before';
+export interface PluginResult {
+	modifiedRequest?: Request;
+	response?: Response;
+	chainable?: boolean;
 };
 
-export type MiddlewarePlugin = () => Promise<Response | null>;
+type PluginReturnTypeIntm = PluginResult | null | undefined;
+type PluginReturnType = Promise<PluginReturnTypeIntm> | PluginReturnTypeIntm
+
+export type MiddlewarePluginResult = PluginResult | null | undefined;
+
+export interface PluginProps {
+	request: Request;
+	middleware: LambdaMiddleware;
+};
+
+export type PluginCallback = (props: PluginProps) => PluginReturnType;
+
+export interface MiddlewarePluginSequenceItem {
+	callback: PluginCallback;
+	sequernce?: 'after' | 'before';
+};
