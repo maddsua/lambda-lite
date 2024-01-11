58d9607996ae2f3be339a059fc0b5c722185c958
maddsua <maddsuadev@proton.me>
2024-01-11 10:09:33 +0200

    Update iplists.ts

---
+++ lib/plugins/serviceAuth.ts
@@ -0,0 +1,108 @@
+import { JSONResponse } from "../rest/jsonResponse.ts";
+import type { MiddlewarePlugin, MiddlewarePluginInstance, SpawnProps } from "../middleware/plugins.ts";
+import type { ServiceConsole } from "../util/console.ts";
+
+const pluginID = 'llp-service_auth';
+
+const uint8sEqual = (a: Uint8Array, b: Uint8Array): boolean => {
+	
+	if (a.length !== b.length) return false;
+
+	for (let idx = 0; idx < a.length; idx++) {
+		if (a[idx] !== b[idx]) return false;
+	}
+
+	return true;
+};
+
+class ServiceAuthPluginImpl implements MiddlewarePluginInstance {
+
+	id = pluginID;
+	tokenHash: Uint8Array;
+	fakeDelayRange?: number;
+	console?: ServiceConsole;
+
+	constructor(init: {
+		tokenHash: Uint8Array;
+		fakeDelayRange?: number;
+		console?: ServiceConsole;
+	}) {
+		this.tokenHash = init.tokenHash;
+		this.fakeDelayRange = init.fakeDelayRange;
+		this.console = init.console;
+	}
+
+	async executeBefore(request: Request) {
+
+		const authBearer = request.headers.get('authorization')?.replace(/^\s*bearer\s+/, '');
+		if (!authBearer?.length) {
+			return {
+				respondWith: new JSONResponse({
+					error_text: 'service access token is required'
+				}, {
+					status: 401,
+					headers: {
+						'WWW-Authenticate': 'Basic realm="API"'
+					}
+				}).toResponse()
+			};
+		}
+
+		const bearerHash = new Uint8Array(await crypto.subtle.digest('sha-256', new TextEncoder().encode(authBearer)));
+
+		if (!uint8sEqual(bearerHash, this.tokenHash)) {
+
+			if (this.fakeDelayRange) {
+				const fakeDelayRange = this.fakeDelayRange;
+				await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fakeDelayRange)));
+			}
+
+			this.console?.error(`[Service token] Invalid service token provided (${authBearer})`);
+
+			return {
+				respondWith: new JSONResponse({
+					error_text: 'invalid service access token'
+				}, { status: 403 }).toResponse()
+			};
+		}
+
+		return null;
+	}
+};
+
+interface InitParams {
+	token: string;
+	fakeDelayRange?: number;
+};
+
+class ServiceAuthPlugin implements MiddlewarePlugin {
+
+	id = pluginID;
+	tokenHash: Uint8Array | null;
+	token: string;
+	fakeDelayRange?: number;
+
+	constructor(init: InitParams) {
+		this.token = init.token;
+		this.tokenHash = null;
+		this.fakeDelayRange = init.fakeDelayRange;
+	}
+
+	async spawn(props: SpawnProps) {
+
+		if (!this.tokenHash) {
+			const hashBuffer = await crypto.subtle.digest('sha-256', new TextEncoder().encode(this.token));
+			this.tokenHash = new Uint8Array(hashBuffer);
+		}
+
+		const useLogging = props.middleware.config.loglevel?.plugins !== false;
+
+		return new ServiceAuthPluginImpl({
+			tokenHash: this.tokenHash,
+			fakeDelayRange: this.fakeDelayRange,
+			console: useLogging ? props.console : undefined
+		});
+	}
+}
+
+export const serviceAuth = (init: InitParams) => new ServiceAuthPlugin(init);
