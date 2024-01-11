7e69c706527153c88b5faaf154586edb48afff65
maddsua <maddsuadev@proton.me>
2024-01-11 06:35:26 +0200

    Update env.ts

--- lib/util/env.ts
+++ lib/util/env.ts
@@ -1,17 +1,32 @@
 
+class ExtendedEnvString {
+
+	string: string;
+
+	constructor(init: string) {
+		this.string = init;
+	}
+
+	asSeparatedList(token?: string) {
+		return this.string.split(token || ',').map(item => item.trim()).filter(item => item.length);
+	}
+
+};
+
 type EnvVariableValueType = 'string' | 'number' | 'boolean' | 'object';
 
 interface TypedEnvVariableCtx {
 	name: string;
-	type?: EnvVariableValueType;
+	type?: EnvVariableValueType | 'extended-string';
 	optional?: true | false;
 };
 
-type EnvValueTypeToType<T extends TypedEnvVariableCtx['type']> = T extends 'string' ?
-	string : T extends 'number' ?
-	number : T extends 'boolean' ?
-	boolean : T extends 'object' ?
-	object : string;
+type EnvValueTypeToType<T extends TypedEnvVariableCtx['type']> = T extends 'string' ? string :
+	T extends 'number' ? number :
+	T extends 'boolean' ? boolean :
+	T extends 'object' ? object :
+	T extends 'extended-string' ? ExtendedEnvString :
+	string;
 
 export type TypedEnvBase = Record<string, TypedEnvVariableCtx>;
 
@@ -34,7 +49,15 @@ export const createEnv = <T extends TypedEnvBase>(schema: T, env: Record<string,
 		}
 
 		try {
-			result[key] = (!ctx.type || ctx.type === 'string') ? value : JSON.parse(value);
+
+			if (ctx.type === 'extended-string') {
+				result[key] = new ExtendedEnvString(value);
+			} else if (!ctx.type || ctx.type === 'string') {
+				result[key] = value;
+			} else {
+				result[key] = JSON.parse(value);
+			}
+
 		} catch (_error) {
 			throw new Error(`[Typed Env]: Failed to parse variable $${ctx.name}`);
 		}
