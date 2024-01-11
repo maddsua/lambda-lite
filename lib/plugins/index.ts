cc80dd085baec64c9a8adf3bc375c054f99750bb
maddsua <maddsuadev@proton.me>
2024-01-11 10:02:44 +0200

    Update index.ts

--- lib/plugins/index.ts
+++ lib/plugins/index.ts
@@ -2,10 +2,12 @@ import { originController } from "./originController.ts";
 import { ratelimiter } from "./ratelimiter.ts";
 import { serviceTokenChecker } from "./serviceTokenChecker.ts";
 import { allowMethods } from "./allowMethods.ts";
+import { ipLists } from "./iplists.ts";
 
 export {
 	originController,
 	ratelimiter,
 	serviceTokenChecker,
 	allowMethods,
+	ipLists,
 };
