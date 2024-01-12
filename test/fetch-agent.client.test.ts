7393ff29e832e116114757dc0e360723ad4b5bb2
maddsua <maddsuadev@proton.me>
2024-01-12 21:19:38 +0200

    Update fetch-agent.client.test.ts

--- test/fetch-agent.client.test.ts
+++ test/fetch-agent.client.test.ts
@@ -2,7 +2,7 @@ import { TypedFetchAgent } from "../lib/rest/fetch.ts";
 import { Schema } from "./functions/typedapi.ts";
 
 const response = await new TypedFetchAgent<Schema>({
-	url: '/',
+	url: 'http://localhost:8080/typedapi',
 	data: {
 		id: 'tset'
 	},
