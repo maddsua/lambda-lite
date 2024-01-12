4d2b066e91f98297b7bc1dadbef2942aab7c3c5b
maddsua <maddsuadev@proton.me>
2024-01-12 20:39:05 +0200

    Update lib.mod.ts

--- lib.mod.ts
+++ lib.mod.ts
@@ -2,7 +2,7 @@
 import type { RouteConfig, RouteHandler } from "./lib/middleware/route.ts";
 import type { MiddlewareOptions } from "./lib/middleware/options.ts";
 import { LambdaMiddleware } from "./lib/middleware/middleware.ts";
-import { TypedResponse, unwrapResponse } from "./lib/rest/response.ts";
+import { TypedResponse, unwrapResponse, type InferResponseType } from "./lib/rest/response.ts";
 import { TypedRequest, unwrapRequest } from "./lib/rest/request.ts";
 import * as plugins from './lib/plugins/index.ts';
 import { createEnv, type TypedEnv } from "./lib/util/env.ts";
@@ -16,6 +16,7 @@ export {
 	unwrapRequest,
 	TypedResponse,
 	unwrapResponse,
+	InferResponseType,
 	plugins,
 	createEnv,
 	TypedEnv,
