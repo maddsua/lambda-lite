e0ec02a08636581a7954705f45299e83438223d7
maddsua <maddsuadev@proton.me>
2024-01-12 19:02:35 +0200

    Create typedapi.ts

---
+++ test/functions/typedapi.ts
@@ -0,0 +1,35 @@
+import { RouteHandler } from "../../deno.mod.ts";
+import { FetchSchema } from "../../lib/rest/typed.ts";
+import { InferResponseType, TypedResponse } from "../../lib/rest/response.ts";
+
+type Schema = FetchSchema<{
+	request: {
+		id: string;
+		headers: {
+			'x-captcha': string
+		}
+	},
+	response: {
+		data: {
+			success: boolean;
+			purpose: string;
+			action: string
+		},
+		headers: {
+			test: string;
+		}
+	}
+}>;
+
+export const handler: RouteHandler = (rq, ctx): InferResponseType<Schema> => {
+
+	return new TypedResponse({
+		success: true,
+		purpose: 'imitates a REST API',
+		action: 'use your imagination'
+	}, {
+		headers: {
+			test: 'test'
+		}
+	});
+};
