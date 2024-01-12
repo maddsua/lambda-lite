922a5663f017bb35f8a0ce572655549d30ab6f64
maddsua <maddsuadev@proton.me>
2024-01-12 19:23:26 +0200

    Create lib.mod.ts

---
+++ lib.mod.ts
@@ -0,0 +1,20 @@
+
+import type { RouteConfig, RouteHandler } from "./lib/middleware/route.ts";
+import type { MiddlewareOptions } from "./lib/middleware/options.ts";
+import { LambdaMiddleware } from "./lib/middleware/middleware.ts";
+import { TypedResponse } from "./lib/rest/response.ts";
+import { TypedRequest } from "./lib/rest/request.ts";
+import * as plugins from './lib/plugins/index.ts';
+import { createEnv, type TypedEnvBase } from "./lib/util/env.ts";
+
+export {
+	RouteHandler,
+	RouteConfig,
+	LambdaMiddleware,
+	MiddlewareOptions,
+	TypedRequest,
+	TypedResponse,
+	plugins,
+	createEnv,
+	TypedEnvBase,
+}
