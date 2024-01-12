df9c46182580cfb9cf336d4fe6e5671b08731dcf
maddsua <maddsuadev@proton.me>
2024-01-12 13:04:47 +0200

    Create response.ts

---
+++ lib/rest/response.ts
@@ -0,0 +1,33 @@
+
+export class TypedResponse<
+	D extends object | null = null,
+	H extends Record<string, string> | undefined = undefined,
+	S extends number | undefined = undefined
+> {
+
+	data: D;
+	headers: H;
+	status: S;
+
+	constructor(data: D, init?: {
+		headers?: H;
+		status?: S;
+	}) {
+		this.data = data;
+		this.headers = init?.headers as H;
+		this.status = init?.status as S;
+	}
+
+	toResponse(): Response {
+		const body = this.data ? JSON.stringify(this.data) : null;
+		const headers = new Headers(this.headers);
+		if (this.data) headers.set('content-type', 'application/json');
+		return new Response(body, { headers, status: this.status });
+	}
+};
+
+export type InferResponseType<T extends {
+	data: object | null;
+	headers?: Record<string, string>;
+	status?: number;
+}> = TypedResponse<T['data'], T['headers'], T['status']>;
