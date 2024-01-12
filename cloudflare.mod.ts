59d6e0090dff48865c3337129ac4907c24f3d60e
maddsua <maddsuadev@proton.me>
2024-01-12 20:38:08 +0200

    Update cloudflare.mod.ts

--- cloudflare.mod.ts
+++ cloudflare.mod.ts
@@ -2,7 +2,7 @@
 import type { RouteConfig, RouteHandler } from "./lib/middleware/route.ts";
 import { LambdaMiddleware } from "./lib/middleware/middleware.ts";
 import { workerFetchHandler, type StartServerOptions } from "./lib/adapters/cloudflare/worker.ts";
-import { TypedResponse } from "./lib/rest/response.ts";
+import { TypedResponse, unwrapResponse } from "./lib/rest/response.ts";
 import { TypedRequest, unwrapRequest } from "./lib/rest/request.ts";
 import { createEnv, type TypedEnv } from "./lib/util/env.ts";
 import * as plugins from './lib/plugins/index.ts';
@@ -10,9 +10,10 @@ import * as plugins from './lib/plugins/index.ts';
 export {
 	RouteHandler,
 	RouteConfig,
-	TypedResponse,
 	TypedRequest,
 	unwrapRequest,
+	TypedResponse,
+	unwrapResponse,
 	LambdaMiddleware,
 	workerFetchHandler,
 	StartServerOptions,
