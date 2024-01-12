f1cf9b98d7bbe8ebc9c97bfe77079a1bdd80c288
maddsua <maddsuadev@proton.me>
2024-01-12 20:38:06 +0200

    Update deno.mod.ts

--- deno.mod.ts
+++ deno.mod.ts
@@ -1,16 +1,17 @@
 import type { RouteConfig, RouteHandler } from "./lib/middleware/route.ts";
 import { startServer } from "./lib/adapters/deno/server.ts";
 import { createEnv, type TypedEnv } from "./lib/util/env.ts";
-import { TypedResponse } from "./lib/rest/response.ts";
+import { TypedResponse, unwrapResponse } from "./lib/rest/response.ts";
 import { TypedRequest, unwrapRequest } from "./lib/rest/request.ts";
 import * as plugins from './lib/plugins/index.ts';
 
 export {
 	RouteHandler,
 	RouteConfig,
-	TypedResponse,
 	TypedRequest,
 	unwrapRequest,
+	TypedResponse,
+	unwrapResponse,
 	startServer,
 	plugins,
 	createEnv,
