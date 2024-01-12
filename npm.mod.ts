4dc83d40929da7e6f05ae501e29485bb7c4af2be
maddsua <maddsuadev@proton.me>
2024-01-12 19:23:46 +0200

    Update npm.mod.ts

--- npm.mod.ts
+++ npm.mod.ts
@@ -1,6 +1,5 @@
 
 import type { RouteConfig, RouteHandler } from "./lib/middleware/route.ts";
-import { JSONResponse } from "./lib/rest/jsonResponse.ts";
 import type { MiddlewareOptions } from "./lib/middleware/options.ts";
 import { LambdaMiddleware } from "./lib/middleware/middleware.ts";
 import * as plugins from './lib/plugins/index.ts';
@@ -9,7 +8,6 @@ import { createEnv, type TypedEnvBase } from "./lib/util/env.ts";
 export {
 	RouteHandler,
 	RouteConfig,
-	JSONResponse,
 	LambdaMiddleware,
 	MiddlewareOptions,
 	plugins,
