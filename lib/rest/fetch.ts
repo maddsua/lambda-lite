e12ccd01f5b4b79319abec53496cec7a91e84720
maddsua <maddsuadev@proton.me>
2024-01-17 03:48:08 +0200

    Update fetch.ts

--- lib/rest/fetch.ts
+++ lib/rest/fetch.ts
@@ -1,27 +1,15 @@
-import { TypedFetchRequest } from "../../deno.mod.ts";
 import { unwrapResponse } from "./response.ts";
 import { FetchSchema } from "./typed.ts";
 import { TypedRequest } from "./request.ts";
 
-export class TypedFetchAgent <T extends FetchSchema<any>> {
+export const typedFetch = async <T extends FetchSchema<any>>(init: T['request'] & { url: string | URL }) => {
 
-	url: string | URL;
-	request: TypedFetchRequest;
+	const request = new TypedRequest(init.url, {
+		headers: init.headers,
+		data: init.data
+	}).toRequest();
 
-	constructor(init: T['request'] & { url: string | URL }) {
-		this.request = init;
-		this.url = init.url;
-	}
+	const response = await fetch(request);
 
-	async fetch() {
-
-		const request = new TypedRequest(this.url, {
-			headers: this.request.headers,
-			data: this.request.data
-		}).toRequest();
-
-		const response = await fetch(request);
-
-		return await unwrapResponse<T>(response);
-	}
+	return await unwrapResponse<T>(response);
 };
