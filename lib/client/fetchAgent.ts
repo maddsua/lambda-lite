1c4b7e753d3742dd900a02b25345dfeac289d88a
maddsua <maddsuadev@proton.me>
2024-01-17 08:35:47 +0200

    Update fetchAgent.ts

--- lib/client/fetchAgent.ts
+++ lib/client/fetchAgent.ts
@@ -1,11 +1,13 @@
+import { InferResponse } from "../../cloudflare.mod.ts";
 import type { FetchSchema, TypedRequestInit, RouterSchema } from "../middleware/typedRouter.ts";
+import { InferRequest } from "../typedrest/request.ts";
 
 interface AgentConfig {
 	endpoint: string;
 };
 
-type QueryResult <T extends FetchSchema<any>> = Promise<T['response']>;
-type QueryAction <T extends FetchSchema<any>> = T['request'] extends never ? () => QueryResult<T> : (opts: T['request']) => QueryResult<T>;
+type QueryReponse <T extends FetchSchema<any>> = Promise<InferResponse<T>>;
+type QueryAction <T extends FetchSchema<any>> = T['request'] extends object ? (opts: InferRequest<T>) => QueryReponse<T> : () => QueryReponse<T>;
 type RouterQueries <T extends RouterSchema<any>> = { [K in keyof T]: QueryAction<T[K]> };
 
 export class TypedFetchAgent <T extends RouterSchema<Record<string, FetchSchema<any>>>> {
