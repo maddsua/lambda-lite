b0209228bfbc793bff417c03d40906285fe9320c
maddsua <maddsuadev@proton.me>
2024-01-17 06:13:40 +0200

    Update lib.mod.ts

--- lib.mod.ts
+++ lib.mod.ts
@@ -4,7 +4,7 @@ import type { RouteConfig, RouteHandler } from "./lib/middleware/route.ts";
 import type { FetchSchema, TypedFetchRequest, TypedFetchResponse } from "./lib/rest/typed.ts";
 import { TypedResponse, unwrapResponse, type InferResponse } from "./lib/rest/response.ts";
 import { TypedRequest, unwrapRequest, type InferRequest } from "./lib/rest/request.ts";
-import { typedFetch } from "./lib/rest/fetch.ts";
+import { typedFetch } from "./lib/client/fetch.ts";
 import * as plugins from './lib/plugins/index.ts';
 import { createEnv, type TypedEnv } from "./lib/util/env.ts";
 
