b7ef7226caa12452301ea7f93836c016f8a419eb
maddsua <maddsuadev@proton.me>
2024-01-17 06:13:41 +0200

    Update deno.mod.ts

--- deno.mod.ts
+++ deno.mod.ts
@@ -3,7 +3,7 @@ import { startServer } from "./lib/adapters/deno/server.ts";
 import { createEnv, type TypedEnv } from "./lib/util/env.ts";
 import type { FetchSchema, TypedFetchRequest, TypedFetchResponse } from "./lib/rest/typed.ts";
 import { TypedResponse, unwrapResponse, type InferResponse } from "./lib/rest/response.ts";
-import { typedFetch } from "./lib/rest/fetch.ts";
+import { typedFetch } from "./lib/client/fetch.ts";
 import { TypedRequest, unwrapRequest, type InferRequest } from "./lib/rest/request.ts";
 import * as plugins from './lib/plugins/index.ts';
 
