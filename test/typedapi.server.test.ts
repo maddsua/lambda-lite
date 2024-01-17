690c1864be69de324f9b36f42b3719714266e98c
maddsua <maddsuadev@proton.me>
2024-01-17 07:33:41 +0200

    Update typedapi.server.test.ts

--- test/typedapi.server.test.ts
+++ test/typedapi.server.test.ts
@@ -1,22 +1,37 @@
 import { LambdaMiddleware } from "../lib/middleware/middleware.ts";
 import { workerFetchHandler } from "../lib/adapters/cloudflare/worker.ts";
-import { ServerRouter, TypedRouter } from "../lib/middleware/typedRouter.ts";
+import { ServerRouter, RouterSchema } from "../lib/middleware/typedRouter.ts";
 import { TypedResponse } from "../lib/typedrest/response.ts";
 
-type RouterType = TypedRouter<{
-	'/*': {
+export type RouterType = RouterSchema<{
+	'action': {
 		response: {
 			data: {
 				message: string;
 			}
 		}
+	},
+	mutation: {
+		request: {
+			data: {
+				id: string
+			}
+		},
+		response: {
+			status: 200 | 400
+		}
 	}
 }>;
 
 const routes: ServerRouter<RouterType> = {
-	'/*': {
+	'action': {
 		handler: () => new TypedResponse({
 			message: 'test'
+		})
+	},
+	mutation: {
+		handler: () => ({
+			status: 200
 		})
 	}
 };
