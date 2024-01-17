10a1f165249ab887079b30b03850610e67344e3d
maddsua <maddsuadev@proton.me>
2024-01-17 20:43:23 +0200

    Update typedapi.client.test.ts

--- test/typedapi.client.test.ts
+++ test/typedapi.client.test.ts
@@ -1,6 +1,6 @@
 
 import { TypedFetchAgent } from "../lib/client/fetchAgent.ts";
-import { RouterType } from "./typedapi.server.test.ts";
+import type { RouterType } from "./typedapi.server.test.ts";
 
 const agent = new TypedFetchAgent<RouterType>({
 	endpoint: 'http://localhost:8080/'
