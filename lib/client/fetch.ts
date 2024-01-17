bb143b40f1a384b621a20d6ea7b464b8b83d112a
maddsua <maddsuadev@proton.me>
2024-01-17 06:13:33 +0200

    Create fetch.ts

---
+++ lib/client/fetch.ts
@@ -0,0 +1,15 @@
+import { unwrapResponse } from "../rest/response.ts";
+import { FetchSchema } from "../rest/typed.ts";
+import { TypedRequest } from "../rest/request.ts";
+
+export const typedFetch = async <T extends FetchSchema<any>>(init: T['request'] & { url: string | URL }) => {
+
+	const request = new TypedRequest(init.url, {
+		headers: init.headers,
+		data: init.data
+	}).toRequest();
+
+	const response = await fetch(request);
+
+	return await unwrapResponse<T>(response);
+};
