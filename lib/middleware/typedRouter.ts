8406c6f0f87b19c74cb731439dd97705853a4d2b
maddsua <maddsuadev@proton.me>
2024-01-17 06:41:15 +0200

    Update typedRouter.ts

--- lib/middleware/typedRouter.ts
+++ lib/middleware/typedRouter.ts
@@ -2,12 +2,12 @@ import { InferResponse } from "../rest/response.ts";
 import { FetchSchema } from "../rest/typed.ts";
 import { RequestContextBase, RouteConfig } from "./router.ts";
 
-export type TypedRouter <T extends Record<string, FetchSchema<any>>> = T;
+export type RouterSchema <T extends Record<string, FetchSchema<any>>> = T;
 
 type TypedRouteCtx <T extends FetchSchema<any>, C extends object = {}> = RouteConfig & {
 	handler: (request: Request, context: RequestContextBase & C) => InferResponse<T> | Promise<InferResponse<T>>;
 };
 
-export type ServerRouter <T extends TypedRouter<any>, C extends object = {}> = {
+export type ServerRouter <T extends RouterSchema<any>, C extends object = {}> = {
 	[K in keyof T]: TypedRouteCtx<T[K], C>;
 };
