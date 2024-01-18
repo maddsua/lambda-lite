142dee33da1519b398a8de0fcbcd7f17e942fc8b
maddsua <maddsuadev@proton.me>
2024-01-18 18:53:39 +0200

    Update deno.mod.ts

--- deno.mod.ts
+++ deno.mod.ts
@@ -1,16 +1,24 @@
-import type { RouteConfig, Handler } from "./lib/middleware/router.ts";
-import { startServer } from "./lib/adapters/deno/server.ts";
-import { createEnv, type TypedEnv } from "./lib/util/env.ts";
-import type { FetchSchema, TypedRequestInit, TypedResponseInit } from "./lib/middleware/typedRouter.ts";
-import { TypedResponse, type InferResponse } from "./lib/typedrest/response.ts";
+import type { MiddlewareOptions } from "./lib/middleware/options.ts";
+import type { RouteConfig } from "./lib/middleware/router.ts";
+import type { Handler, TypedHandler } from "./lib/routes/handlers.ts";
+import { TypedRequest, type TypedRequestInit } from "./lib/restapi/typedRequest.ts";
+import { TypedResponse, type TypedResponseInit } from "./lib/restapi/typedResponse.ts";
+import type { FetchSchema } from "./lib/routes/schema.ts";
+import { LambdaMiddleware } from "./lib/middleware/middleware.ts";
 import { typedFetch } from "./lib/client/fetch.ts";
 import { TypedFetchAgent } from "./lib/client/fetchAgent.ts";
-import { TypedRequest, unwrapRequest } from "./lib/typedrest/request.ts";
 import * as plugins from './lib/plugins/index.ts';
+import { createEnv, type TypedEnv } from "./lib/util/env.ts";
+
+import { startServer, type StartServerOptions } from "./lib/adapters/deno/server.ts";
 
 export {
 
+	LambdaMiddleware,
+	MiddlewareOptions,
+
 	Handler,
+	TypedHandler,
 	RouteConfig,
 
 	FetchSchema,
@@ -18,10 +26,7 @@ export {
 	TypedResponseInit,
 
 	TypedRequest,
-	unwrapRequest,
-
 	TypedResponse,
-	InferResponse,
 
 	typedFetch,
 	TypedFetchAgent,
@@ -32,4 +37,5 @@ export {
 	plugins,
 
 	startServer,
+	StartServerOptions
 }
