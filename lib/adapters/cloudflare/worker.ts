4157240e56b86bb596538ca19ec9214df5763750
maddsua <maddsuadev@proton.me>
2024-01-19 00:23:02 +0200

    Update worker.ts

--- lib/adapters/cloudflare/worker.ts
+++ lib/adapters/cloudflare/worker.ts
@@ -1,6 +1,6 @@
 import type { MiddlewareOptions } from "../../middleware/options.ts";
 import type { BasicRouter } from "../../middleware/router.ts";
-import type { NetworkInfo } from "../../routes/handlers.ts";
+import type { NetworkInfo } from "../../middleware/context.ts";
 import { LambdaMiddleware } from '../../middleware/middleware.ts';
 
 export interface WorkerStartOptions extends MiddlewareOptions {
