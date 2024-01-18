b39985fbecfdf602d02a0e17a9e47643f5e04c21
maddsua <maddsuadev@proton.me>
2024-01-18 15:49:02 +0200

    Update cloudflare.mod.ts

--- cloudflare.mod.ts
+++ cloudflare.mod.ts
@@ -2,7 +2,7 @@ import { LambdaMiddleware } from "./lib/middleware/middleware.ts";
 import type { MiddlewareOptions } from "./lib/middleware/options.ts";
 import type { RouteConfig, RouteHandler } from "./lib/middleware/router.ts";
 import type { FetchSchema, TypedRequestInit, TypedResponseInit } from "./lib/middleware/typedRouter.ts";
-import { TypedResponse, unwrapResponse, type InferResponse } from "./lib/typedrest/response.ts";
+import { TypedResponse, type InferResponse } from "./lib/typedrest/response.ts";
 import { TypedRequest, unwrapRequest, type InferRequest } from "./lib/typedrest/request.ts";
 import { typedFetch } from "./lib/client/fetch.ts";
 import { TypedFetchAgent } from "./lib/client/fetchAgent.ts";
@@ -28,7 +28,6 @@ export {
 
 	TypedResponse,
 	InferResponse,
-	unwrapResponse,
 	
 	typedFetch,
 	TypedFetchAgent,
