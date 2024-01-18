6df7c6bca28a16e941ab1c55e20ed0b9bb1d6591
maddsua <maddsuadev@proton.me>
2024-01-18 18:26:49 +0200

    Update rest.ts

--- lib/api/rest.ts
+++ lib/api/rest.ts
@@ -1,4 +1,3 @@
-import type { FetchSchema } from "../routes/schema.ts";
 
 export interface SerializableResponse {
 	toResponse(): Response;
@@ -122,31 +121,5 @@ export class TypedResponse<
 		if (this.data) headers.set('content-type', 'application/json');
 
 		return new Response(body, { headers, status: this.status });
-	}
-};
-
-export class LambdaRequest <T extends FetchSchema<any>> extends Request {
-
-	constructor(init: Request) {
-		super(init);
-	}
-
-	async unwrap(): Promise<T['request']> {
-		const { searchParams } = new URL(this.url);
-
-		if (this.method === 'GET') return {
-			headers: Object.fromEntries(this.headers.entries()),
-			query: Object.fromEntries(searchParams.entries()),
-		};
-	
-		const contentIsJSON = this.headers.get('content-type')?.toLowerCase()?.includes('json');
-		const requestData = contentIsJSON ? await this.json().catch(() => null) : null;
-		if (contentIsJSON && !requestData) throw new Error('Invalid typed request: no data');
-	
-		return {
-			data: requestData,
-			headers: Object.fromEntries(this.headers.entries()),
-			query: Object.fromEntries(searchParams.entries())
-		};
 	}
 };
