7060405eb9024ab1d0cad4a8cb2939f13e5f89dd
maddsua <maddsuadev@proton.me>
2024-01-22 11:37:36 +0200

    Update handlers.ts

--- lib/routes/handlers.ts
+++ lib/routes/handlers.ts
@@ -1,9 +1,9 @@
-import type { FetchSchema, TypedRequestInit } from "../routes/schema.ts";
+import type { FetchSchema } from "../routes/schema.ts";
 import type { LambdaRequest, SerializableResponse, TypedResponse } from "../middleware/rest.ts";
 import type { LambdaContext } from "../middleware/context.ts";
 
 export type HandlerResponse = SerializableResponse | Response;
-export type Handler<C extends object = {}> = (request: LambdaRequest, context: LambdaContext & C) => Promise<HandlerResponse> | HandlerResponse;
+export type Handler<C extends object = {}> = (request: LambdaRequest<any>, context: LambdaContext & C) => Promise<HandlerResponse> | HandlerResponse;
 
 type InferResponse <T extends FetchSchema<any>> = TypedResponse<
 	T['response']['data'],
