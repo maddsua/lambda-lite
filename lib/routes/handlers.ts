e2c40df239ea2defa469ef3100e7768d437df134
maddsua <maddsuadev@proton.me>
2024-01-22 11:08:58 +0200

    Update handlers.ts

--- lib/routes/handlers.ts
+++ lib/routes/handlers.ts
@@ -1,9 +1,9 @@
-import type { FetchSchema } from "../routes/schema.ts";
+import type { FetchSchema, TypedRequestInit } from "../routes/schema.ts";
 import type { LambdaRequest, SerializableResponse, TypedResponse } from "../middleware/rest.ts";
 import type { LambdaContext } from "../middleware/context.ts";
 
 export type HandlerResponse = SerializableResponse | Response;
-export type Handler<C extends object = {}> = (request: LambdaRequest<any>, context: LambdaContext & C) => Promise<HandlerResponse> | HandlerResponse;
+export type Handler<C extends object = {}> = (request: LambdaRequest, context: LambdaContext & C) => Promise<HandlerResponse> | HandlerResponse;
 
 type InferResponse <T extends FetchSchema<any>> = TypedResponse<
 	T['response']['data'],
@@ -11,4 +11,4 @@ type InferResponse <T extends FetchSchema<any>> = TypedResponse<
 	T['response']['status']
 >;
 type TypedHandlerResponse <T extends FetchSchema<any>> = InferResponse<T> | Promise<InferResponse<T>>;
-export type TypedHandler <T extends FetchSchema<any>, C extends object = {}> = (request: LambdaRequest<T>, context: LambdaContext & C) => TypedHandlerResponse<T>;
+export type TypedHandler <T extends FetchSchema<any>, C extends object = {}> = (request: LambdaRequest<T['request']>, context: LambdaContext & C) => TypedHandlerResponse<T>;
