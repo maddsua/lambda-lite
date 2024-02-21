5f2b391103d6767e27fe21b357178d6e6a8e1f87
maddsua <maddsuadev@proton.me>
2024-02-21 19:37:41 +0200

    imported typed env package

--- examples/basic/main.ts
+++ examples/basic/main.ts
@@ -1,8 +1,17 @@
 import { startServer } from "../../mod.ts";
+import { createEnv } from "../deps.ts";
+
+const env = createEnv({
+	port: {
+		name: 'PORT',
+		type: 'number',
+		optional: true
+	}
+}, Deno.env.toObject());
 
 await startServer({
 	serve: {
-		port: 8080,
+		port: env.port || 8080,
 	},
 	healthcheckPath: '/health',
 	//errorLogDetail: 'log',
--- examples/extended/deps.ts
+++
@@ -1,5 +0,0 @@
-import { React } from 'https://raw.githubusercontent.com/maddsua/jsxml/3.3.0/mod.ts';
-
-export {
-	React
-}
