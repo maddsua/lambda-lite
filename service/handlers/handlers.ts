e53fd3c823f5fa9d026e9ee074a0a5094e6c3388
maddsua <maddsuadev@proton.me>
2024-03-22 18:03:28 +0200

    Create handlers.ts

---
+++ service/handlers/handlers.ts
@@ -0,0 +1,9 @@
+import { ServiceContext } from "./context.ts";
+import type { SerializableResponse } from "./responses.ts";
+
+export type HandlerResponse = SerializableResponse | Response;
+export type MaybePromise <T> = T | Promise<T>;
+
+export type HandlerFunction <C extends object = {}> =
+	(request: Request, context: ServiceContext & C) =>
+		MaybePromise<HandlerResponse>;
